df0665d9a70565457e49f6d76488a199fc0501bf
r-adi09 <rajeshwariadi98@gmail.com>
2022-10-17 01:38:57 -0400

    a5 final commit

---
+++ public/labs/a5/todos/TodoItem.js
@@ -0,0 +1,19 @@
+/*const TodoItem = (todo) => {
+    return(`
+        <li>
+            ${todo.title}
+            (${todo.status})
+        </li>
+    `);
+}
+export default TodoItem;*/
+const TodoItem = (todo) => {
+    return(`
+        <li>
+            <input type="checkbox"
+                ${todo.done ? 'checked' : ''}/>
+            ${todo.title}
+            (${todo.status})
+        </li>`);
+}
+export default TodoItem;
