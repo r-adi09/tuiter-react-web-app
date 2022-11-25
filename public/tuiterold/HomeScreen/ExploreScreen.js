310517f46592f7d0dc5f0321fdcf0d82d5f9c94b
r-adi09 <rajeshwariadi98@gmail.com>
2022-11-25 14:41:01 -0500

    ESLint errors

--- public/tuiterold/HomeScreen/ExploreScreen.js
+++ public/tuiterold/HomeScreen/ExploreScreen.js
@@ -1,19 +1,20 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
-import ExploreComponent from "./ExploreComponent.js";
-import WhoToFollowList from "../WhoToFollowList/index.js";
+import PostSummaryList from "../HomeScreenPostSummaryList/index.js";
+import postList from "./PostList.js";
 (function ($) {
     $('#wd-explore').append(`
         <div class="row">
             <div class="col-xxl-2 col-xl-2 col-lg-1 col-md-2 col-sm-2">
-                ${NavigationSidebar('explore')}
+                ${NavigationSidebar('home')}
             </div>
             <div class="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10">
-                ${ExploreComponent()}
+                ${postList()}
             </div>
-            <div class="col-xxl-4 col-xl-4 col-lg-3 d-none d-lg-block">
-                ${WhoToFollowList()}
+            <div class="col-xxl-4 col-xl-4 col-lg-4 d-none d-lg-block">
+                ${PostSummaryList()}
             </div>
-            
         </div>
     `);
+
+// eslint-disable-next-line no-undef
 })($);
--- public/tuiterold/explore/index.js
+++ public/tuiterold/explore/index.js
@@ -1,19 +1,20 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
-import PostSummaryList from "../HomeScreenPostSummaryList/index.js";
-import postList from "./PostList.js";
+import ExploreComponent from "./ExploreComponent.js";
+import WhoToFollowList from "../WhoToFollowList/index.js";
 (function ($) {
     $('#wd-explore').append(`
         <div class="row">
             <div class="col-xxl-2 col-xl-2 col-lg-1 col-md-2 col-sm-2">
-                ${NavigationSidebar('home')}
+                ${NavigationSidebar('explore')}
             </div>
             <div class="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10">
-                ${postList()}
+                ${ExploreComponent()}
             </div>
-            <div class="col-xxl-4 col-xl-4 col-lg-4 d-none d-lg-block">
-                ${PostSummaryList()}
+            <div class="col-xxl-4 col-xl-4 col-lg-3 d-none d-lg-block">
+                ${WhoToFollowList()}
             </div>
+            
         </div>
     `);
-
+// eslint-disable-next-line no-undef
 })($);
