c1a5354e2ae7f63ae730a71399e9204d41da899a
r-adi09 <rajeshwariadi98@gmail.com>
2022-11-25 00:45:21 -0500

    fixes

--- src/services/tuits-thunks.js
+++ src/services/tuits-thunks.js
@@ -1,45 +1,30 @@
 import {createAsyncThunk}
     from "@reduxjs/toolkit";
 import * as service from '../services/tuits-service';
-export const CREATE_TUIT = 'CREATE_TUIT';
-export const FIND_ALL_TUITS = 'FIND_ALL_TUITS';
-export const UPDATE_TUIT = 'UPDATE_TUIT';
-export const DELETE_TUIT = 'DELETE_TUIT';
 
-export const createTuit = async (dispatch, tuit) => {
-    const newTuit = await service.createTuit(tuit);
-    dispatch({
-        type: CREATE_TUIT,
-        tuit:newTuit
-    });
-}
-
-export const updateTuit = async (dispatch, tuit) => {
-    console.log('actions',tuit);
-    const status = await service.updateTuit(tuit);
-    dispatch({
-        type: UPDATE_TUIT,
-        tuit
-    });
-}
-
-export const deleteTuit = async (dispatch, tuit) => {
-    const response = await service.deleteTuit(tuit);
-    dispatch({
-        type: DELETE_TUIT,
-        tuit
-    })
-}
-
-export const findAllTuits = async (dispatch) => {
-    const tuits = await service.findAllTuits();
-    dispatch({
-        type: FIND_ALL_TUITS,
-        tuits
-    });
-}
-
-export const findTuitsThunk = createAsyncThunk(
+export const FIND_ALL_TUITS = createAsyncThunk(
     'tuits/findTuits', async () =>
         await service.findAllTuits()
 )
+
+export const DELETE_TUIT = createAsyncThunk(
+    'tuits/deleteTuit',
+    async (tuitId) => {
+        console.log(tuitId)
+        await service.deleteTuit(tuitId)
+        return tuitId
+    })
+
+export const CREATE_TUIT = createAsyncThunk(
+    'tuits/createTuit',
+    async (tuit) => {
+        await service.createTuit(tuit)
+        return tuit
+    })
+
+export const UPDATE_TUIT =
+    createAsyncThunk(
+        'tuits/updateTuit',
+        async (tuit) =>
+            await service.updateTuit(tuit)
+    )
