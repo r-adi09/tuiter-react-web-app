300018c0c0263e8fa4e3d21c05d58fde0428fb7c
r-adi09 <rajeshwariadi98@gmail.com>
2022-11-25 02:28:57 -0500

    testing with heroku 2.0

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 //const TUITS_API = 'http://localhost:4000/api/tuits';
-const TUITS_API = 'https://tuiter-node-server-app.herokuapp.com/api/tuits';
+const TUITS_API = 'https://tuiter-node-server-app-r-adi09.herokuapp.com/api/tuits';
 //const TUITS_API = `${API_BASE}/tuits`;
 
 console.log('tuits_api',TUITS_API);
