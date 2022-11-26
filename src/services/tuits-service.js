fefbcac75028687708eb44ec2f29d64f10bdea95
r-adi09 <rajeshwariadi98@gmail.com>
2022-11-26 15:17:35 -0500

    env changes

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = "https://tuiter-node-server-app-r-adi09.herokuapp.com/api";
 //const TUITS_API = 'http://localhost:4000/api/tuits';
 //const TUITS_API = 'https://tuiter-node-server-app-r-adi09.herokuapp.com/api/tuits';
 const TUITS_API = `${API_BASE}/tuits`;
